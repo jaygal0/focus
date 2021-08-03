4eaa0f4cf0dbacfbfd137adeac852fdefc6241e0
jaygal <jaygal@Joshuas-MBP.lan>
2021-08-03 16:14:45 +0200

    changed url to next.js

--- utils/url.js
+++ utils/url.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:3000'
-// const url = 'https://focus-jaygal0.vercel.app/'
+// const url = 'http://localhost:3000'
+const url = 'https://focus-jaygal0.vercel.app/'
 
 export default url
